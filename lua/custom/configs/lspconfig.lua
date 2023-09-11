7fe141c80da23711c02d100b6a4f99987a544722
Wojciech Kosierkiewicz <wojtekkosierkiewicz@gmail.com>
2023-09-11 03:52:01 +0200

    rust support

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,27 @@
+local configs = require "plugins.configs.lspconfig"
+local on_attach = configs.on_attach
+local capabilities = configs.capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { "html","cssls", "clangd", "gopls" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig.rust_analyzer.setup({
+  on_attach = on_attach,
+  capabilities = capabilities,
+  filetypes = {"rust"},
+  root_dir = lspconfig.util.root_pattern("Cargo.toml"),
+  settings = {
+    ['rust-analyzer'] = {
+      cargo = {
+        allFeatures = true,
+      },
+    },
+  },
+})
